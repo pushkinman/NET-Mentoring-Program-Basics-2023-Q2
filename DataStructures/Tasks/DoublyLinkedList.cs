2714ba33e2455ad81b7013419f1472d49b517ad4
Anton Pushkin <anton.pushkin2@gmail.com>
2023-06-14 18:59:12 +0300

    Complete task 1 for "Data Structures" module

--- DataStructures/Tasks/DoublyLinkedList.cs
+++ DataStructures/Tasks/DoublyLinkedList.cs
@@ -7,41 +7,159 @@ namespace Tasks
 {
     public class DoublyLinkedList<T> : IDoublyLinkedList<T>
     {
-        public int Length => throw new NotImplementedException();
+        private DoublyLinkedListNode<T> _head;
+        private DoublyLinkedListNode<T> _tail;
+        private int _count;
+
+        public int Length => _count;
 
         public void Add(T e)
         {
-            throw new NotImplementedException();
+            var newNode = new DoublyLinkedListNode<T>(e);
+
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                _tail.Next = newNode;
+                newNode.Previous = _tail;
+                _tail = newNode;
+            }
+
+            _count++;
         }
 
         public void AddAt(int index, T e)
         {
-            throw new NotImplementedException();
+            if (index < 0 || index > _count)
+                throw new IndexOutOfRangeException();
+
+            if (index == _count)
+            {
+                Add(e);
+                return;
+            }
+
+            var newNode = new DoublyLinkedListNode<T>(e);
+
+            if (index == 0)
+            {
+                newNode.Next = _head;
+                _head.Previous = newNode;
+                _head = newNode;
+            }
+            else
+            {
+                var current = GetNodeAt(index);
+                var previous = current.Previous;
+
+                newNode.Next = current;
+                newNode.Previous = previous;
+                previous.Next = newNode;
+                current.Previous = newNode;
+            }
+
+            _count++;
         }
 
         public T ElementAt(int index)
         {
-            throw new NotImplementedException();
+            var node = GetNodeAt(index);
+            return node.Value;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = _head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
         }
 
         public void Remove(T item)
         {
-            throw new NotImplementedException();
+            var current = _head;
+            while (current != null)
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Value, item))
+                {
+                    RemoveNode(current);
+                    return;
+                }
+
+                current = current.Next;
+            }
         }
 
         public T RemoveAt(int index)
         {
-            throw new NotImplementedException();
+            var nodeToRemove = GetNodeAt(index);
+            RemoveNode(nodeToRemove);
+            return nodeToRemove.Value;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        private DoublyLinkedListNode<T> GetNodeAt(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new IndexOutOfRangeException();
+
+            var current = _head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        private void RemoveNode(DoublyLinkedListNode<T> node)
+        {
+            var previous = node.Previous;
+            var next = node.Next;
+
+            if (previous == null)
+            {
+                _head = next;
+            }
+            else
+            {
+                previous.Next = next;
+                node.Previous = null;
+            }
+
+            if (next == null)
+            {
+                _tail = previous;
+            }
+            else
+            {
+                next.Previous = previous;
+                node.Next = null;
+            }
+
+            _count--;
+        }
+    }
+
+    public class DoublyLinkedListNode<T>
+    {
+        public T Value { get; }
+        public DoublyLinkedListNode<T> Next { get; internal set; }
+        public DoublyLinkedListNode<T> Previous { get; internal set; }
+
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
         }
     }
 }
