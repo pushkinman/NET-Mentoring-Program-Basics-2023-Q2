40e1e1972ce3968aa160c9a04d193947774d49fb
Anton Pushkin <anton.pushkin2@gmail.com>
2023-06-15 10:16:30 +0300

    Complete task 2 for "Data Structures" module

--- DataStructures/Tasks/HybridFlowProcessor.cs
+++ DataStructures/Tasks/HybridFlowProcessor.cs
@@ -1,28 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 using Tasks.DoNotChange;
 
 namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private DoublyLinkedList<T> _list;
+
+        public HybridFlowProcessor()
+        {
+            _list = new DoublyLinkedList<T>();
+        }
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (_list.Length == 0)
+                throw new InvalidOperationException("The flow is empty.");
+
+            T item = _list.ElementAt(0);
+            _list.RemoveAt(0);
+            return item;
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _list.Add(item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (_list.Length == 0)
+                throw new InvalidOperationException("The flow is empty.");
+
+            T item = _list.ElementAt(_list.Length - 1);
+            _list.RemoveAt(_list.Length - 1);
+            return item;
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _list.Add(item);
         }
     }
 }
