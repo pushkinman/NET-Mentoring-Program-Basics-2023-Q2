ce2ffd5a893759ab8e5cc1b63b5809555ab3a217
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-12 20:05:51 +0300

    Add BinaryNumbers test implementation

--- UnitTesting/BinaryNumbers.Tests/BinaryNumbersTests.cs
+++ UnitTesting/BinaryNumbers.Tests/BinaryNumbersTests.cs
@@ -1,16 +1,90 @@
 namespace BinaryNumbers.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class BinaryNumbersTests
     {
+        private BinaryNumbers binaryNumbers;
+
         [SetUp]
         public void Setup()
         {
+            binaryNumbers = new BinaryNumbers();
         }
 
         [Test]
-        public void Test1()
+        public void CountValidSequences_Length4_ReturnsExpectedResult()
         {
-            Assert.Pass();
+            // Arrange
+            int length = 4;
+            int expectedResult = 8;
+
+            // Act
+            int result = binaryNumbers.CountValidSequences(length);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CountValidSequences_Length5_ReturnsExpectedResult()
+        {
+            // Arrange
+            int length = 5;
+            int expectedResult = 13;
+
+            // Act
+            int result = binaryNumbers.CountValidSequences(length);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CountValidSequences_Length10_ReturnsExpectedResult()
+        {
+            // Arrange
+            int length = 10;
+            int expectedResult = 144;
+
+            // Act
+            int result = binaryNumbers.CountValidSequences(length);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CountValidSequences_Length6_ReturnsExpectedResult()
+        {
+            // Arrange
+            int length = 6; // Change the length as needed
+            int expectedResult = 21; // Change the expected result as needed
+
+            // Act
+            int result = binaryNumbers.CountValidSequences(length);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void CountValidSequences_LengthZero_ThrowsArgumentException()
+        {
+            // Arrange
+            int length = 0;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => binaryNumbers.CountValidSequences(length));
+        }
+
+        [Test]
+        public void CountValidSequences_NegativeLength_ThrowsArgumentException()
+        {
+            // Arrange
+            int length = -5;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => binaryNumbers.CountValidSequences(length));
         }
     }
 }
