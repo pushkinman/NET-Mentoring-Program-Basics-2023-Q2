357f085ea3663db71be48404b3f413624da81804
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-12 19:36:57 +0300

    Add 'OddEven' class implementation in a new project

---
+++ UnitTesting/OddEven/OddEven.cs
@@ -0,0 +1,42 @@
+﻿
+public class OddEven
+{
+    public string PrintNumber(int number)
+    {
+        if (IsEven(number))
+        {
+            return "Even";
+        }
+        else if (IsPrime(number))
+        {
+            return "Prime";
+        }
+        else
+        {
+            return "Odd";
+        }
+    }
+
+    private bool IsEven(int number)
+    {
+        return number % 2 == 0;
+    }
+
+    private bool IsPrime(int number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
