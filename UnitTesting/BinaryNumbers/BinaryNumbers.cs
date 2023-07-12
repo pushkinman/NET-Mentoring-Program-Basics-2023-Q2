b257ce0e3bedb852f8e57de419b28f265fd7e192
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-12 20:06:15 +0300

    Add BinaryNumbers implementation

---
+++ UnitTesting/BinaryNumbers/BinaryNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace BinaryNumbers
+{
+    public class BinaryNumbers
+    {
+        public int CountValidSequences(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Invalid input. The length of the sequence should be greater than 0.");
+            }
+
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n];
+        }
+    }
+}
