7e695f6c5b94ba0f708c37eed83c2a235cd5512d
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-12 19:51:29 +0300

    Add implementation for CalcStats

---
+++ UnitTesting/CalcStats/CalcStats.cs
@@ -0,0 +1,45 @@
+﻿namespace CalcStats
+{
+    public class CalcStats
+    {
+        public int MinValue(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Invalid input. The array cannot be null or empty.");
+            }
+
+            return numbers.Min();
+        }
+
+        public int MaxValue(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Invalid input. The array cannot be null or empty.");
+            }
+
+            return numbers.Max();
+        }
+
+        public int NumberOfElements(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentException("Invalid input. The array cannot be null.");
+            }
+
+            return numbers.Length;
+        }
+
+        public double AverageValue(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                throw new ArgumentException("Invalid input. The array cannot be null or empty.");
+            }
+
+            return numbers.Average();
+        }
+    }
+}
