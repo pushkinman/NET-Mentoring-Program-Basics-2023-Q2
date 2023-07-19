424d079d9c616c58872da54e8792f35c7209d3cc
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-19 10:14:57 +0300

    Add database project and tables
    SQL Fundamentals

---
+++ Database/Database/dbo/Tables/Employee.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AddressId] INT NOT NULL, 
+    [PersonId] INT NOT NULL, 
+    [CompanyName] NVARCHAR(20) NOT NULL, 
+    [Position] NVARCHAR(30) NULL, 
+    [EmployeeName] NCHAR(100) NULL, 
+    CONSTRAINT [FK_Employee_Address] FOREIGN KEY ([AddressId]) REFERENCES [Address]([Id]), 
+    CONSTRAINT [FK_Employee_Person] FOREIGN KEY ([PersonId]) REFERENCES [Person]([Id])
+)
