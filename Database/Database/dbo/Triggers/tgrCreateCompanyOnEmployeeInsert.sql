19317f05cb66b745bd0d2cf913f740e8fc83345d
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-21 11:22:55 +0300

    Add trigger
    SQL Fundamentals

---
+++ Database/Database/dbo/Triggers/tgrCreateCompanyOnEmployeeInsert.sql
@@ -0,0 +1,24 @@
+﻿CREATE TRIGGER CreateCompanyOnEmployeeInsert
+ON Employee
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @AddressId int;
+    DECLARE @CompanyName nvarchar(20);
+
+    -- Get the inserted employee's address ID
+    SELECT @AddressId = AddressId FROM inserted;
+
+    -- Get the inserted employee's company name
+    SELECT @CompanyName = CompanyName FROM inserted;
+
+    -- Check if a company with the same name already exists
+    IF NOT EXISTS (SELECT * FROM Company WHERE Name = @CompanyName)
+    BEGIN
+        -- Insert the new company with the employee's address
+        DECLARE @CompanyId int;
+        INSERT INTO Company (Name, AddressId)
+        VALUES (@CompanyName, @AddressId);
+        SET @CompanyId = SCOPE_IDENTITY();
+    END
+END
