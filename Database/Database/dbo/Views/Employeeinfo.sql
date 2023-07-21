67bf23a5fe9f09710ab00aa5e861eaf2e7d14e5c
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-21 10:33:36 +0300

    Add a view
    SQL Fundamentals

---
+++ Database/Database/dbo/Views/Employeeinfo.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW EmployeeInfo AS
+SELECT e.Id AS EmployeeId,
+       COALESCE(e.EmployeeName, p.FirstName + ' ' + p.LastName) AS EmployeeFullName,
+       a.ZipCode + '_' + COALESCE(a.State, '') + ', ' + COALESCE(a.City, '') + '-' + COALESCE(a.Street, '') AS EmployeeFullAddress,
+       e.CompanyName + '(' + COALESCE(e.Position, '') + ')' AS EmployeeCompanyInfo
+FROM Employee e
+INNER JOIN Person p ON e.PersonId = p.Id
+INNER JOIN Address a ON e.AddressId = a.Id
