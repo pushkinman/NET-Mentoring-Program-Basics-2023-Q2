33c364ee82006c1f9e1fa6d362f9866a8a4fa1e8
Anton Pushkin <anton.pushkin2@gmail.com>
2023-07-26 21:16:39 +0300

    Add task 3
    OOP Fundamentals

--- LibararySoftware/LibararySoftware/FileDocumentRepository.cs
+++ LibararySoftware/LibararySoftware/FileDocumentRepository.cs
@@ -1,25 +1,38 @@
 ﻿using LibararySoftware.Interfaces;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace LibararySoftware
 {
     public class FileDocumentRepository : IDocumentRepository
     {
         private string documentsFolderPath;
+        private Dictionary<string, Document> documentCache;
+        private Dictionary<string, TimeSpan> cacheExpirationTimes;
 
         public FileDocumentRepository(string folderPath)
         {
             documentsFolderPath = folderPath;
+            documentCache = new Dictionary<string, Document>();
+            cacheExpirationTimes = new Dictionary<string, TimeSpan>
+        {
+            { "Patent", TimeSpan.FromMinutes(30) },
+            { "Book", TimeSpan.FromMinutes(60) },
+            { "LocalizedBook", TimeSpan.FromMinutes(45) },
+            { "Magazine", TimeSpan.FromMinutes(15) }
+        };
         }
 
         public Document GetDocumentByNumber(string documentNumber)
         {
+            
+            if (documentCache.TryGetValue(documentNumber, out var cachedDocument))
+            {
+                if (!IsCacheExpired(documentNumber))
+                    return cachedDocument;
+                else
+                    documentCache.Remove(documentNumber);
+            }
+
             string filePath = Path.Combine(documentsFolderPath, $"{documentNumber}.json");
 
             if (!File.Exists(filePath))
@@ -29,18 +42,49 @@ namespace LibararySoftware
             JObject documentData = JObject.Parse(jsonContent);
 
             string documentType = (string)documentData["Type"];
+            Document document = null;
 
-            // Deserialize based on the document type
-            if (documentType == "Patent")
-                return documentData.ToObject<Patent>();
-            else if (documentType == "Book")
-                return documentData.ToObject<Book>();
-            else if (documentType == "LocalizedBook")
-                return documentData.ToObject<LocalizedBook>();
-            else if (documentType == "Magazine")
-                return documentData.ToObject<Magazine>();
+            switch (documentType)
+            {
+                case "Patent":
+                    document = documentData.ToObject<Patent>();
+                    break;
+                case "Book":
+                    document = documentData.ToObject<Book>();
+                    break;
+                case "LocalizedBook":
+                    document = documentData.ToObject<LocalizedBook>();
+                    break;
+                case "Magazine":
+                    document = documentData.ToObject<Magazine>();
+                    break;
+                default:
+                    return null;
+            }
 
-            return null;
+            if (cacheExpirationTimes.ContainsKey(documentType))
+            {
+                documentCache[documentNumber] = document;
+            }
+
+            return document;
+        }
+
+        private bool IsCacheExpired(string documentNumber)
+        {
+            if (documentCache.TryGetValue(documentNumber, out var cachedDocument))
+            {
+                string documentType = cachedDocument.GetType().Name;
+
+                if (cacheExpirationTimes.TryGetValue(documentType, out var cacheExpirationTime))
+                {
+                    DateTime cacheTime = (DateTime)cachedDocument.GetType().GetProperty("DatePublished").GetValue(cachedDocument);
+                    DateTime expirationTime = cacheTime.Add(cacheExpirationTime);
+                    return DateTime.Now > expirationTime;
+                }
+            }
+
+            return true;
         }
     }
 
