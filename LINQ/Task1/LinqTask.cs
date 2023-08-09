87b0c4f20853f890694fd8ac9f051295dbe7ff97
Anton Pushkin <anton.pushkin2@gmail.com>
2023-08-09 09:20:01 +0300

    Add implementation for tests

--- LINQ/Task1/LinqTask.cs
+++ LINQ/Task1/LinqTask.cs
@@ -9,7 +9,7 @@ namespace Task1
     {
         public static IEnumerable<Customer> Linq1(IEnumerable<Customer> customers, decimal limit)
         {
-            throw new NotImplementedException();
+            return customers.Where(c => c.Orders.Sum(o => o.Total) > limit);
         }
 
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2(
@@ -17,7 +17,7 @@ namespace Task1
             IEnumerable<Supplier> suppliers
         )
         {
-            throw new NotImplementedException();
+            return customers.Select(cust => (cust, suppliers.Where(s => s.Country == cust.Country && s.City == cust.City)));
         }
 
         public static IEnumerable<(Customer customer, IEnumerable<Supplier> suppliers)> Linq2UsingGroup(
@@ -25,47 +25,80 @@ namespace Task1
             IEnumerable<Supplier> suppliers
         )
         {
-            throw new NotImplementedException();
+            return customers
+                .GroupJoin(
+                    suppliers,
+                    cust => new { cust.Country, cust.City },
+                    supp => new { supp.Country, supp.City },
+                    (cust, supplierGroup) => (cust, supplierGroup)
+                );
         }
 
         public static IEnumerable<Customer> Linq3(IEnumerable<Customer> customers, decimal limit)
         {
-            throw new NotImplementedException();
+            var filteredCustomers = customers
+        .Where(c => c.Orders.Sum(o => o.Total) > limit)
+        .ToList();
+
+            return filteredCustomers;
         }
 
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq4(
             IEnumerable<Customer> customers
         )
         {
-            throw new NotImplementedException();
+            var result = customers
+                .Where(c => c.Orders.Any())
+                .Select(c => (c, dateOfEntry: c.Orders.Min(o => o.OrderDate)))
+                .ToList();
+
+            return result;
         }
 
         public static IEnumerable<(Customer customer, DateTime dateOfEntry)> Linq5(
             IEnumerable<Customer> customers
         )
         {
-            throw new NotImplementedException();
+            return customers
+                .Where(c => c.Orders.Any())
+                .Select(c => (c, c.Orders.Min(o => o.OrderDate)))
+                .OrderBy(t => t.Item2);
         }
 
         public static IEnumerable<Customer> Linq6(IEnumerable<Customer> customers)
         {
-            throw new NotImplementedException();
+            var filteredCustomers = customers
+                .Where(c => !IsNumericPostalCode(c.PostalCode) || string.IsNullOrEmpty(c.Region) || !HasOperatorCode(c.Phone))
+                .ToList();
+
+            return filteredCustomers;
+        }
+
+        private static bool IsNumericPostalCode(string postalCode)
+        {
+            return !string.IsNullOrEmpty(postalCode) && postalCode.All(char.IsDigit);
+        }
+
+        private static bool HasOperatorCode(string phoneNumber)
+        {
+            return !string.IsNullOrEmpty(phoneNumber) && phoneNumber.Contains("(") && phoneNumber.Contains(")");
         }
 
         public static IEnumerable<Linq7CategoryGroup> Linq7(IEnumerable<Product> products)
         {
-            /* example of Linq7result
-
-             category - Beverages
-	            UnitsInStock - 39
-		            price - 18.0000
-		            price - 19.0000
-	            UnitsInStock - 17
-		            price - 18.0000
-		            price - 19.0000
-             */
-
-            throw new NotImplementedException();
+            return products
+                .GroupBy(p => p.Category)
+                .Select(group => new Linq7CategoryGroup
+                {
+                    Category = group.Key,
+                    UnitsInStockGroup = group
+                        .GroupBy(p => p.UnitsInStock)
+                        .Select(unitGroup => new Linq7UnitsInStockGroup
+                        {
+                            UnitsInStock = unitGroup.Key,
+                            Prices = unitGroup.Select(p => p.UnitPrice)
+                        })
+                });
         }
 
         public static IEnumerable<(decimal category, IEnumerable<Product> products)> Linq8(
@@ -75,19 +108,50 @@ namespace Task1
             decimal expensive
         )
         {
-            throw new NotImplementedException();
+            var cheapProducts = products.Where(p => p.UnitPrice <= cheap).ToList();
+            var middleProducts = products.Where(p => p.UnitPrice > cheap && p.UnitPrice <= middle).ToList();
+            var expensiveProducts = products.Where(p => p.UnitPrice > middle && p.UnitPrice <= expensive).ToList();
+
+            var result = new List<(decimal category, IEnumerable<Product> products)>
+                {
+                    (cheap, cheapProducts),
+                    (middle, middleProducts),
+                    (expensive, expensiveProducts)
+                };
+
+            return result;
         }
 
         public static IEnumerable<(string city, int averageIncome, int averageIntensity)> Linq9(
-            IEnumerable<Customer> customers
-        )
+            IEnumerable<Customer> customers)
         {
-            throw new NotImplementedException();
+            var result = customers
+                .GroupBy(c => c.City)
+                .Select(group => new
+                {
+                    City = group.Key,
+                    TotalIncome = group.Sum(c => c.Orders.Sum(o => o.Total)),
+                    TotalIntensity = group.Sum(c => c.Orders.Length),
+                    CustomerCount = group.Count()
+                })
+                .Select(info => (
+                    city: info.City,
+                    averageIncome: info.CustomerCount > 0 ? (int)Math.Round(info.TotalIncome / info.CustomerCount) : 0,
+                    averageIntensity: info.CustomerCount > 0 ? info.TotalIntensity / info.CustomerCount : 0
+                ))
+                .ToList();
+
+            return result;
         }
 
         public static string Linq10(IEnumerable<Supplier> suppliers)
         {
-            throw new NotImplementedException();
+            return string.Join("", suppliers
+                .Select(s => s.Country)
+                .Distinct()
+                .OrderBy(c => c.Length)
+                .ThenBy(c => c)
+            );
         }
     }
 }
