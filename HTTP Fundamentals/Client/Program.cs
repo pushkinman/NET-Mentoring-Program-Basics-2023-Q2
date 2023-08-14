1e049b9e46107582a8600a16b07e79030c15e73f
Anton Pushkin <anton.pushkin2@gmail.com>
2023-08-14 16:17:05 +0300

    Complete Tasks
    HTTP Fundamentals

---
+++ HTTP Fundamentals/Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            // Task 1: URL
+            string myNameResponse = await client.GetStringAsync("http://localhost:8888/MyName/");
+            Console.WriteLine(myNameResponse);
+
+            // Task 2: HTTP status messages
+            await MakeRequestAndPrintStatus(client, "http://localhost:8888/Information/");
+            await MakeRequestAndPrintStatus(client, "http://localhost:8888/Success/");
+            await MakeRequestAndPrintStatus(client, "http://localhost:8888/Redirection/");
+            await MakeRequestAndPrintStatus(client, "http://localhost:8888/ClientError/");
+            await MakeRequestAndPrintStatus(client, "http://localhost:8888/ServerError/");
+
+            // Task 3: Header
+            HttpResponseMessage myNameByHeaderResponse = await client.GetAsync("http://localhost:8888/MyNameByHeader/");
+            if (myNameByHeaderResponse.Headers.TryGetValues("X-MyName", out var headerValues))
+            {
+                foreach (var value in headerValues)
+                {
+                    Console.WriteLine("X-MyName Header Value: " + value);
+                }
+            }
+
+            // Task 4: Cookies
+            HttpResponseMessage myNameByCookiesResponse = await client.GetAsync("http://localhost:8888/MyNameByCookies/");
+            if (myNameByCookiesResponse.Headers.TryGetValues("Set-Cookie", out var cookieValues))
+            {
+                foreach (var value in cookieValues)
+                {
+                    if (value.StartsWith("MyName="))
+                    {
+                        Console.WriteLine("MyName Cookie Value: " + value);
+                    }
+                }
+            }
+        }
+    }
+
+    static async Task MakeRequestAndPrintStatus(HttpClient client, string url)
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        Console.WriteLine($"{url} - Status Code: {(int)response.StatusCode} {response.ReasonPhrase}");
+    }
+}
---
+++ HTTP Fundamentals/Listener/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StartListener();
+    }
+
+    static void StartListener()
+    {
+        using (HttpListener listener = new HttpListener())
+        {
+            listener.Prefixes.Add("http://localhost:8888/");
+            listener.Start();
+            Console.WriteLine("Listener started. Waiting for requests...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HandleRequest(context);
+            }
+        }
+    }
+
+    static void HandleRequest(HttpListenerContext context)
+    {
+        string resourcePath = context.Request.Url.LocalPath.Trim('/');
+        string responseText = "";
+
+        if (resourcePath == "MyName")
+        {
+            responseText = GetMyName();
+        }
+        else if (resourcePath == "Information")
+        {
+            responseText = "Informational response";
+            context.Response.StatusCode = 200; // Using a valid status code for demonstration
+        }
+        else if (resourcePath == "Success")
+        {
+            responseText = "Success response";
+            context.Response.StatusCode = 200;
+        }
+        else if (resourcePath == "Redirection")
+        {
+            responseText = "Redirection response";
+            context.Response.StatusCode = 302; // Using a valid redirection status code
+            context.Response.Headers.Add("Location", "http://example.com");
+        }
+        else if (resourcePath == "ClientError")
+        {
+            responseText = "Client error response";
+            context.Response.StatusCode = 400;
+        }
+        else if (resourcePath == "ServerError")
+        {
+            responseText = "Server error response";
+            context.Response.StatusCode = 500;
+        }
+        else if (resourcePath == "MyNameByHeader")
+        {
+            responseText = GetMyName();
+            context.Response.Headers.Add("X-MyName", "Your Name");
+        }
+        else if (resourcePath == "MyNameByCookies")
+        {
+            responseText = GetMyName();
+            context.Response.Cookies.Add(new Cookie("MyName", "Your Name"));
+        }
+
+        byte[] buffer = Encoding.UTF8.GetBytes(responseText);
+        context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+        context.Response.Close();
+    }
+
+    static string GetMyName()
+    {
+        return "Your Name";
+    }
+}
